f50ee616e182f4405de708fa04a54aeb3929789c
Marc-Antoine <marc-antoine.champagne.2@ulaval.ca>
2024-03-13 17:56:19 -0400

    atelier complete

---
+++ db_scripts/rollback_1.sql
@@ -0,0 +1,6 @@
+DROP TABLE band;
+
+ALTER TABLE musician DROP COLUMN role;
+ALTER TABLE musician DROP COLUMN bandName;
+ALTER TABLE musician RENAME COLUMN musicianName TO singerName;
+ALTER TABLE musician RENAME TO singer;
